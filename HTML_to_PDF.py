6d2cba316a2659a7085d3a7e6f3839854ec2e846
ebruun <edvard.bruun@ce.gatech.edu>
2026-01-28 21:26:56 -0500

    Finished L3_2 Part 2

--- HTML_to_PDF.py
+++ HTML_to_PDF.py
@@ -128,7 +128,7 @@ async def html_to_pdf(html_path: str, pdf_path: str):
                 if (!page.querySelector('.pdf-header-center')) {
                   const header = document.createElement('div');
                   header.className = 'pdf-header-center';
-                  header.textContent = 'Lecture 2: 01/23';
+                  header.textContent = 'Lecture 3: 01/30';
                   page.appendChild(header);
                 }
 
