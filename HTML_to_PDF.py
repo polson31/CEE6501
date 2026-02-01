8d22f6e165dfa00acfe5c3b4e2f41dccd8a5d9c9
ebruun <edvard.bruun@ce.gatech.edu>
2026-02-01 18:31:37 -0500

    done re-org for L4_1

--- HTML_to_PDF.py
+++ HTML_to_PDF.py
@@ -128,7 +128,7 @@ async def html_to_pdf(html_path: str, pdf_path: str):
                 if (!page.querySelector('.pdf-header-center')) {
                   const header = document.createElement('div');
                   header.className = 'pdf-header-center';
-                  header.textContent = 'Lecture 3: 01/30';
+                  header.textContent = 'Lecture 4: 02/06';
                   page.appendChild(header);
                 }
 
